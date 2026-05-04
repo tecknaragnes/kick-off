60c3c079552f96d2f113ad7c7db44382c73747d4
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-05-04 16:37:14 +0200

    detaljfix

--- pixabay.js
+++ pixabay.js
@@ -1,10 +1,14 @@
-const params = new URLSearchParams({
-    // api_key: ,
-    q = "",
-    image_type: "photo",
-    orientation: ""
-});
+export const fetchPixabay = async (query) => {
+    const params = new URLSearchParams({
+        // api_key: ,
+        q = "",
+        image_type: "photo",
+        orientation: ""
+    });
 
-console.log("visar full URL,", `https://pixabay.com/api/?${params}`);
-const response = await fetch(`https://pixabay.com/api/?${params}`);
-const data = await response.json(); 
+    console.log("full URL,", `https://pixabay.com/api/?${params}`);
+}
+
+
+// const response = await fetch(`https://pixabay.com/api/?${params}`);
+// const data = await response.json(); 
