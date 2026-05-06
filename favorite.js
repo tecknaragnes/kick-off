b20c042abf2afd36d4eeafb7c6bbd202123dcbc7
zakariyamahamud10-gmail <zakariyamahamud10@gmail.com>
2026-05-06 19:29:55 +0200

    börjat med favorite.js och favorite.html

--- favorite.js
+++ favorite.js
@@ -1,1 +1,11 @@
 // Localstorage, man kan spara sina favorit aktiviteter 
+import { renderActivities } from "./render.js";
+
+const savedFavorites = localStorage.getItem("favorites");
+
+if (savedFavorites) {
+    const favorites = JSON.parse(savedFavorites);
+    renderActivities(favorites);
+} else {
+    renderActivities([])
+}
