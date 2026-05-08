57b4c256baf6d8361c704b6001dbd3bcc6af0826
zakariyamahamud10-gmail <zakariyamahamud10@gmail.com>
2026-05-08 16:59:50 +0200

    hdj

--- activities.js
+++ activities.js
@@ -1,3 +1,6 @@
 //renderingen, filtrering imortera det
 import { renderActivities } from "./rendering.js";
 import { fetchPixabay } from "./pixabay.js";
+import { listenerEvents } from "./filter.js"
+
+listenerEvents()
