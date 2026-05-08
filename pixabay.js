a2729d7cf933681e4086f279ca7c3913193cc310
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-05-08 16:59:53 +0200

    Pixabay try/catch

--- pixabay.js
+++ pixabay.js
@@ -3,14 +3,20 @@ export const fetchPixabay = async (query) => {
         key: pixakey,
         q: query,
         image_type: "photo",
-        orientation: ""
+        orientation: "",
+        lang: "sv"
     });
 
     console.log("Pixabay-sökning:", `https://pixabay.com/api/?${params}`);
+
+    try {
+        const response = await fetch(`https://pixabay.com/api/?${params}`);
+        const pixaPics = await response.json();
+    } catch (error) {
+        console.error("Fel vid hämtning från Pixabay:", error);
+        return [];
+    }
 }
 
-
-// const response = await fetch(`https://pixabay.com/api/?${params}`);
-// const data = await response.json();
-
 // kolla efter description i datan för aktiviteten, och använd den som query i pixabay
+// matcha en lista med idn från pixabay till idn i datan, och lägg till en bild-url i renderingen för varje matchning
