1352da3917beaac759435df71b655ae279f579b5
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-05-19 10:06:49 +0200

    Typ klar med responsivitet på akivitetssidan

--- JS/activities.js
+++ JS/activities.js
@@ -11,7 +11,7 @@ foldSection.style.display = "none";
 foldSection.classList.replace("visible", "hidden");
 const foldBtn = document.querySelector("#fold-btn");
 
-foldBtn.addEventListener("click", () => {
+foldBtn.addEventListener("click", () => { //går det att fixa så att den visas när skärmen är bredare än 1500px? Alltså ta bort knappen för fäll ut/in och bara visa
     if (foldSection.classList == "hidden") {
         foldSection.classList.replace("hidden", "visible");
         foldSection.style.display = "flex";
