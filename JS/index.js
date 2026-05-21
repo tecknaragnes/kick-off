65ee3b977038feea42f1fe3d7a1a6711c9c8376a
zakariyamahamud10-gmail <zakariyamahamud10@gmail.com>
2026-05-21 15:55:30 +0200

    fixat startsidan, visar populäraste aktiv, och renderingen, ska testa att lägga in bilderna, ksk ikonerna så den liknar activities.js

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,18 @@
 import { fetchActivities } from "./api.js";
+import { getImageForActivity } from "./pixabay.js";
+
 
 ///Visa rekommenderade aktiviteter på startsidan (mest poppis)
 /// återanvända renderActivoties eller skapa en ny för bara main, 
 //återanvända api.js hämtning
 
+//let allActivities = [];
+
+
 const popularActivities = document.getElementById("popular-activities")
 
-async function loadPopularActivities () {
-    const data = await fetchActivities ({
-        sort: rating-Highlight,
-        descriptions: [
-            "Bowlinghall",
+const allowedDescriptions2 = [
+  "Bowlinghall",
   "Gokart",
   "Golfbana",
   "Nöjespark",
@@ -20,7 +22,57 @@ async function loadPopularActivities () {
   "Paintballcenter",
   "Hälsocenter",
   "Biograf"
+];
 
-        ]
+async function loadPopularActivities() {
+    const data = await fetchActivities({
+        sort: "rating-high",
+        descriptions: allowedDescriptions2,
     })
+
+    console.log(data.payload)
+
+    const activities = data.payload ?? [];
+    const popularList= activities.slice(0,5);
+    renderPopActivities (popularList)
+
+    console.log(popularList)
+
+
+
+
 }
+
+loadPopularActivities();
+
+function renderPopActivities (popularList) {
+    popularActivities.innerHTML = "";
+
+  for (const activity of popularList) {
+    popularActivities.innerHTML += `
+      <div class="activity-card">
+        <div class="act-flex-card">
+          <div class="act-img"></div>
+
+          <div class="act-flex-info">
+            <h3>${activity.name}</h3>
+            <p>(${activity.description})</p>
+            <p>
+              <img src="SVG/location.svg" alt="">
+              ${activity.city}, ${activity.province}
+            </p>
+            <p>${activity.price_range} kr</p>
+            <p>Betyg: ${activity.rating}</p>
+          </div>
+        </div>
+
+        <div class="act-symbols">
+          <a href="HTML/details.html?id=${activity.id}">Läs mer</a>
+        </div>
+      </div>
+    `;
+  }
+
+}
+
+//renderPopActivities()
