86052a17b7853842f60884ec63f962ffa78bfd1e
Gabriel Elfström <ge222ik@student.lnu.se>
2026-05-26 14:28:17 +0200

    quiz

--- JS/spel.js
+++ JS/spel.js
@@ -40,5 +40,43 @@ quiz.addEventListener("submit", function(event) {
 
     let answers = [answer1.value, answer2.value, answer3.value, answer4.value, answer5.value, foodCheckbox.checked, drinkCheckbox.checked];
     console.log(answers);
+
+    let filter1 = [];
+
+    if (answers[0] == 1 || answers[0] == 2 || answers[0] == 3) {
+        filter1 += "low";
+    }
+    if (answers[0] == 2 || answers[0] == 3 || answers[0] == 4) {
+        filter1 += "medium";
+    }
+    if (answers[0] == 3 || answers[0] == 4 || answers[0] == 5) {
+        filter1 += "high";
+    }
+
+    let filter2 = [];
+
+    if (answers[1] == 1 || answers[1] == 2 || answers[1] == 3) {
+        filter2 += "low";
+    }
+    if (answers[1] == 2 || answers[1] == 3 || answers[1] == 4) {
+        filter2 += "medium";
+    }
+    if (answers[1] == 3 || answers[1] == 4 || answers[1] == 5) {
+        filter2 += "high";
+    }
+
+        let filter3 = [];
+
+    if (answers[2] == 1 || answers[2] == 2 || answers[2] == 3) {
+        filter3 += "low";
+    }
+    if (answers[2] == 2 || answers[2] == 3 || answers[2] == 4) {
+        filter3 += "medium";
+    }
+    if (answers[2] == 3 || answers[2] == 4 || answers[2] == 5) {
+        filter3 += "high";
+    }
+
+    console.log(filter1);
 });
 
